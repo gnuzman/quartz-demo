87f5a1581edf56369ddfb75ee3b2ed06253b164d
gnuzman <94644378@qq.com>
2017-08-25 10:08:23 +0800

    data params

--- src/main/java/com/opencodez/controller/TestController.java
+++ src/main/java/com/opencodez/controller/TestController.java
@@ -3,6 +3,7 @@ package com.opencodez.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.swagger.annotations.Api;
 import org.quartz.JobKey;
 import org.quartz.SchedulerException;
 import org.quartz.TriggerKey;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,7 @@ import com.opencodez.quartz.jobs.DynamicJob;
 import com.opencodez.util.AppUtil;
 import com.opencodez.util.PropertiesUtils;
 
+@Api(tags = "Quartz")
 @RestController
 public class TestController {
 	
@@ -29,7 +32,7 @@ public class TestController {
 	@Autowired
 	private SchedulerFactoryBean schedFactory;
 	
-	@RequestMapping("/getval")
+	@GetMapping("/getval")
     public String getVal(@RequestParam(value="key", defaultValue="World") String key) {
 		Map<String, String> mapOfKeyValue = new HashMap<String, String>();
 		mapOfKeyValue.put(key, PropertiesUtils.getProperty(key));
@@ -37,19 +40,35 @@ public class TestController {
 		return AppUtil.getBeanToJsonString(mapOfKeyValue);
     }
 	
-	@RequestMapping("/schedule")
+	@GetMapping("/schedule")
 	public String schedule() {
 		String scheduled = "Job is Scheduled!!";
 		try {
 			JobDetailFactoryBean jdfb = ConfigureQuartz.createJobDetail(DynamicJob.class);
 			jdfb.setBeanName("dynamicJobBean");
 			jdfb.afterPropertiesSet();
+
+			jdfb.getJobDataMap().put("url", "http://baidu.com");
 			
 			SimpleTriggerFactoryBean stfb = ConfigureQuartz.createTrigger(jdfb.getObject(),5000L);
 			stfb.setBeanName("dynamicJobBeanTrigger");
 			stfb.afterPropertiesSet();
 			
 			schedFactory.getScheduler().scheduleJob(jdfb.getObject(), stfb.getObject());
+
+			//////////////////////////////////////////////////////////////////////////////////////////////////
+
+			JobDetailFactoryBean jdfb2 = ConfigureQuartz.createJobDetail(DynamicJob.class);
+			jdfb2.setBeanName("dynamicJobBean2");
+			jdfb2.afterPropertiesSet();
+
+			jdfb2.getJobDataMap().put("url", "http://sina.com");
+
+			SimpleTriggerFactoryBean stfb2 = ConfigureQuartz.createTrigger(jdfb2.getObject(),5000L);
+			stfb2.setBeanName("dynamicJobBeanTrigger2");
+			stfb2.afterPropertiesSet();
+
+			schedFactory.getScheduler().scheduleJob(jdfb2.getObject(), stfb2.getObject());
 			
 		} catch (Exception e) {
 			scheduled = "Could not schedule a job. " + e.getMessage();
@@ -57,7 +76,7 @@ public class TestController {
 		return scheduled;
 	}
 	
-	@RequestMapping("/unschedule")
+	@GetMapping("/unschedule")
 	public String unschedule() {
 		String scheduled = "Job is Unscheduled!!";
 		TriggerKey tkey = new TriggerKey("dynamicJobBeanTrigger");
