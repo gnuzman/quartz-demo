86bb5fc7a8babc9693926a45fe8ff37b5f592adb
gnuzman <94644378@qq.com>
2017-08-25 17:25:39 +0800

    new

--- src/main/java/com/opencodez/controller/TestController.java
+++ src/main/java/com/opencodez/controller/TestController.java
@@ -1,8 +1,9 @@
 package com.opencodez.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.opencodez.configuration.ConfigureQuartz;
+import com.opencodez.quartz.jobs.DynamicJob;
+import com.opencodez.util.AppUtil;
+import com.opencodez.util.PropertiesUtils;
 import org.quartz.JobKey;
 import org.quartz.SchedulerException;
 import org.quartz.TriggerKey;
@@ -12,63 +13,61 @@ import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.opencodez.configuration.ConfigureQuartz;
-import com.opencodez.quartz.jobs.DynamicJob;
-import com.opencodez.util.AppUtil;
-import com.opencodez.util.PropertiesUtils;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
-	
-	@Value("${con.key2}")
-	String conKey2;
-	
-	@Autowired
-	private SchedulerFactoryBean schedFactory;
-	
-	@GetMapping("/getval")
-    public String getVal(@RequestParam(value="key", defaultValue="World") String key) {
-		Map<String, String> mapOfKeyValue = new HashMap<String, String>();
-		mapOfKeyValue.put(key, PropertiesUtils.getProperty(key));
-		mapOfKeyValue.put("con.key2", conKey2);
-		return AppUtil.getBeanToJsonString(mapOfKeyValue);
+
+    @Value("${con.key2}")
+    String conKey2;
+
+    @Autowired
+    private SchedulerFactoryBean schedFactory;
+
+    @GetMapping("/getval")
+    public String getVal(@RequestParam(value = "key", defaultValue = "World") String key) {
+        Map<String, String> mapOfKeyValue = new HashMap<String, String>();
+        mapOfKeyValue.put(key, PropertiesUtils.getProperty(key));
+        mapOfKeyValue.put("con.key2", conKey2);
+        return AppUtil.getBeanToJsonString(mapOfKeyValue);
     }
-	
-	@GetMapping("/schedule")
-	public String schedule() {
-		String scheduled = "Job is Scheduled!!";
-		try {
-			JobDetailFactoryBean jdfb = ConfigureQuartz.createJobDetail(DynamicJob.class);
-			jdfb.setBeanName("dynamicJobBean");
-			jdfb.afterPropertiesSet();
-			
-			SimpleTriggerFactoryBean stfb = ConfigureQuartz.createTrigger(jdfb.getObject(),5000L);
-			stfb.setBeanName("dynamicJobBeanTrigger");
-			stfb.afterPropertiesSet();
-			
-			schedFactory.getScheduler().scheduleJob(jdfb.getObject(), stfb.getObject());
-			
-		} catch (Exception e) {
-			scheduled = "Could not schedule a job. " + e.getMessage();
-		}
-		return scheduled;
-	}
-	
-	@GetMapping("/unschedule")
-	public String unschedule() {
-		String scheduled = "Job is Unscheduled!!";
-		TriggerKey tkey = new TriggerKey("dynamicJobBeanTrigger");
-		JobKey jkey = new JobKey("dynamicJobBean"); 
-		try {
-			schedFactory.getScheduler().unscheduleJob(tkey);
-			schedFactory.getScheduler().deleteJob(jkey);
-		} catch (SchedulerException e) {
-			scheduled = "Error while unscheduling " + e.getMessage();
-		}
-		return scheduled;
-	}
+
+    @GetMapping("/schedule")
+    public String schedule(@RequestParam(value = "jobName") String jobName,
+                           @RequestParam(value = "triggerName") String triggerName) {
+        String scheduled = "Job is Scheduled!!";
+        try {
+            JobDetailFactoryBean jdfb = ConfigureQuartz.createJobDetail(DynamicJob.class);
+            jdfb.setBeanName("dynamicJobBean");
+            jdfb.afterPropertiesSet();
+
+            SimpleTriggerFactoryBean stfb = ConfigureQuartz.createTrigger(jdfb.getObject(), 5000L);
+            stfb.setBeanName("dynamicJobBeanTrigger");
+            stfb.afterPropertiesSet();
+
+            schedFactory.getScheduler().scheduleJob(jdfb.getObject(), stfb.getObject());
+
+        } catch (Exception e) {
+            scheduled = "Could not schedule a job. " + e.getMessage();
+        }
+        return scheduled;
+    }
+
+    @GetMapping("/unschedule")
+    public String unschedule() {
+        String scheduled = "Job is Unscheduled!!";
+        TriggerKey tkey = new TriggerKey("dynamicJobBeanTrigger");
+        JobKey jkey = new JobKey("dynamicJobBean");
+        try {
+            schedFactory.getScheduler().unscheduleJob(tkey);
+            schedFactory.getScheduler().deleteJob(jkey);
+        } catch (SchedulerException e) {
+            scheduled = "Error while unscheduling " + e.getMessage();
+        }
+        return scheduled;
+    }
 }
